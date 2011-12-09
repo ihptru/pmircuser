5f56eae11f3638deaf45000fdf78136174ac01fd
Igor Popov <ihptru@gmail.com>
2011-12-09 23:20:52 +0400

    basic interface

--- index.py
+++ index.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
+import cgi, os
+import style
 
 print("""Content-Type: text/html\n""")
-print("hello")
+content = "<head><title>Private Message to IRC user</title>"
+content += "<style>"+style.style+"</style>"
+content += "</head>"
+
+print(content)
+
+f = cgi.FieldStorage()
+user = ""
+
+if "user" in f:
+    user = f["user"].value
+
+content = "<table class='center'>"
+
+content += "<form method=GET action=''>"
+content += "<tr><td>Your nickname (optional)</td><td><input type='text' name='nickname'></td></tr>"
+content += "<tr><td>Target IRC nickname</td><td><input type='text' name='target' value='"+user+"'></td></tr>"
+content += "<tr><td>IRC Server</td><td><input type='text' name='server' value='irc.freenode.net'></td></tr>"
+content += "<tr><td>IRC Port</td><td><input type='text' name='server' value='6667'></td></tr>"
+
+# text area
+content += "<label for='message'>Your Message</label><br />"
+content += "<textarea id='message' name='message' rows='10' cols='40' tabindex='4'></textarea>"
+content += "</p>"
+content += "<p class='no-border' style='text-align:center'>"
+content += "<input class='button' type='submit' value='Submit' tabindex='5'/>"
+content += "</p>"
+
+content += "</form></td></tr></table>"
+
+print(content)
+
---
+++ style.py
@@ -0,0 +1,85 @@
+style = """table {
+	border-collapse: collapse;
+	margin: 10px 20px;		
+}
+
+tr { background: #130d0a }
+
+th, td {
+	text-align: left;			
+	border-width: 1px;
+  	border-style: solid;
+}
+
+th {
+	background-color: #45561B; 
+	padding: .8em 1em;	
+  	border-color: #647C28 #586A22 #43511A #5A6F23;	
+	font-weight: bold;
+	color: #fff;
+}
+
+td {
+	border-color: #231A14;
+	padding: .7em 1em;
+	margin-left: auto;   margin-right: auto;
+	text-align:center;
+}
+
+table.center {
+	margin-left:auto; 
+	margin-right:auto;
+	border:2px solid;
+	width:600px;
+	font-size:12px;
+	margin-top: 40px;
+	text-align:center;
+}
+
+input, select, textarea {
+	margin: 5px 0;
+	padding: 7px 5px;
+	border-width: 1px;
+	border-style: solid;
+	border-color: #fff; 	
+	background-color: #1E150D;
+	font: 11px 'Lucida Grande', Verdana, Helvetica, Sans-Serif;
+	color: #925D3A;
+}
+
+input:focus, select:focus, textarea:focus {
+	background-color: #934A00;
+	border: 1px solid #BF6000;
+	color: #fff;
+}
+
+input.button { 
+	margin-top: 25px;
+	padding: 5px 10px;
+	border: 1px solid #c96111;
+	background: #e2721b url(../images/button-bg.png) repeat-x;	
+	text-decoration: none;
+	text-transform: uppercase;
+	font-size: 10px;
+	font-weight: bold;
+	line-height: 20px;	
+	color: #fff;	
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+	text-align: center;
+}
+
+input.button:hover {
+	border: 1px solid #2f4c13;
+	background: #4a781d url(../images/button-hover-bg.png) repeat-x;
+}
+
+body {
+	font: 12px/170% 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, Sans-Serif;
+	color: #91807F; 
+	margin: 0; padding: 0; 
+	background: #1d140f;
+	text-align: center;
+}
+"""
